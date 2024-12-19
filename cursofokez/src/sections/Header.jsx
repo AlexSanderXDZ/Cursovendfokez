4b85ebfe4592cc43fb3533b2c5b906d553de48ca
Adryan Alex. <adryan.alex16@gmail.com>
2024-12-18 22:26:16 -0300

    a

--- cursofokez/src/sections/Header.jsx
+++ cursofokez/src/sections/Header.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
+import '../styles/Header.css';
 
 function Header() {
   return (
     <header className="header">
       <div className="container">
-        <h1>CursinhodoFokez</h1>
+        <div className="logo">Nome do Curso</div>
         <nav>
-          <ul>
+          <ul className="nav-links">
             <li><a href="#about">Sobre</a></li>
             <li><a href="#courses">Cursos</a></li>
             <li><a href="#contact">Contato</a></li>
           </ul>
         </nav>
+        <button className="cta-button">Inscreva-se</button>
       </div>
     </header>
   )
