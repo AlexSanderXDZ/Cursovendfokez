52562e323c1ba4c1c78a23166f70e5de708f2060
Adryan Alex. <adryan.alex16@gmail.com>
2024-12-21 17:14:38 -0300

    att style

--- cursofokez/src/sections/Header.jsx
+++ cursofokez/src/sections/Header.jsx
@@ -4,18 +4,18 @@ import '../styles/Header.css';
 function Header() {
   return (
     <header className="header">
-      <div className="container">
-        <div className="logo">Nome do Curso</div>
-        <nav>
-          <ul className="nav-links">
-            <li><a href="#about">Sobre</a></li>
-            <li><a href="#courses">Cursos</a></li>
-            <li><a href="#contact">Contato</a></li>
-          </ul>
-        </nav>
-        <button className="cta-button">Inscreva-se</button>
-      </div>
-    </header>
+    <div className="container">
+      <div className="logo">CondoView</div>
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li><a href="#about">Sobre</a></li>
+          <li><a href="#services">Serviços</a></li>
+          <li><a href="#contact">Contato</a></li>
+        </ul>
+      </nav>
+      <button className="cta-button">Acesse Agora</button>
+    </div>
+  </header>
   )
 }
 
