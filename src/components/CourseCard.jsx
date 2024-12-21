51d79ec090bbe51638177c0c79cfe0c7c0312463
Adryan Alex. <adryan.alex16@gmail.com>
2024-12-21 17:37:15 -0300

    nd relevante

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/CourseCard.css';
 
 function CourseCard({ title, description }) {
   return (
--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import '../styles/Main.css';
+import Slider from "react-slick";
 
 function Main() {
   return (
